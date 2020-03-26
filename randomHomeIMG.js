e55c577d7a4751aaa36b1dcedb2e3e6189525937
r3dbob <50021572+r3dbob@users.noreply.github.com>
2020-03-26 11:34:40 +0100

    added randomVideo to homepage

--- randomHomeIMG.js
+++ randomHomeIMG.js
@@ -1,8 +1,12 @@
-window.onload = choosePic;
+window.onload = choose;
 
-var myPix = new Array("img/walls/0.png", "img/walls/1.jpg", "img/walls/2.jpg", "img/walls/3.jpg", "img/walls/4.jpg", "img/walls/5.jpg", "img/walls/6.jpg", "img/walls/7.jpg");
+var myPix = new Array("img/walls/0.png", "img/walls/1.jpg", "img/walls/2.jpg", "img/walls/3.jpg", "img/walls/4.jpg", "img/walls/5.jpg", "img/walls/6.jpg", "img/walls/7.jpg", "img/walls/8.jpg");
 
-function choosePic() {
-     var randomNum = Math.floor(Math.random() * myPix.length);
+var myVid = new Array("img/video/RADIO.mp4", "img/video/CONTROL.mp4", "img/video/ASFTTU.mp4", "img/video/FACES.mp4", "img/video/MATREND.mp4", "img/video/ROBOT_2.mp4", "img/video/RADIO.mp4", );
+
+function choose() {
+    var randomNum = Math.floor(Math.random() * myPix.length);
      document.getElementById("homeIMG").src = myPix[randomNum];
+    var randomNum2 = Math.floor(Math.random() * myVid.length);
+     document.getElementById("homeVID").src = myVid[randomNum2];
 }
